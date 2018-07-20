e60abcb5d34a073da9da2db9f76c011f22137eac
Garnett Puett <gpuett@gmail.com>
2018-07-20 08:38:17 -0700

    pass props down through BarList and Bar components

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -3,16 +3,24 @@ import { fetchBarList } from './../actions';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
-function Bar({dispatch}) {
+function Bar(props) {
   return(
     <div>
-      
+      <h3>{props.name}</h3>
+      <h4>{props.deal}</h4>
+      <h4>{props.happyHour}</h4>
+      <h6>{props.address}</h6>
+      <h6>{props.phone}</h6>
     </div>
   );
 }
 
 Bar.propTypes = {
-  dispatch: PropTypes.func
+  name: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  deal: PropTypes.string.isRequired,
+  happyHour: PropTypes.string.isRequired
 }
 
-export default connect()(Bar);
+export default Bar;
--- src/components/BarList.jsx
+++ src/components/BarList.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Bar from './Bar';
+import PropTypes from 'prop-types';
 
-function BarList() {
+function BarList(props) {
   return(
     <div>
-      <Bar />
+      <hr/>
+      {props.barList.map((bar) =>
+        <Bar name={bar.name}
+          address={bar.address}
+          phone={bar.phone}
+          deal={bar.deal}
+          happyHour={bar.happyHour}
+          key={bar.id}/>
+      )}
     </div>
   );
 }
+
+BarList.propTypes = {
+  barList: PropTypes.array,
+  currentRouterPath: PropTypes.string
+};
 
 export default BarList;
