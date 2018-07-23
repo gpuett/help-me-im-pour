4ae303e2e56d9b0228879f386616e1d8bd6af56c
Garnett Puett <gpuett@gmail.com>
2018-07-23 16:35:03 -0700

    add clock to current deals component

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function Current() {
+
+  function liveTime() {
+    return new Date().toLocaleTimeString();
+    // let now = new Date().toLocaleTimeString();
+    // return setInterval(now, 1000);
+  }
+
   return(
     <div>
+      <h3>Current time: {liveTime()}</h3>
       <h3>Current Happy Hours:</h3>
     </div>
   );
